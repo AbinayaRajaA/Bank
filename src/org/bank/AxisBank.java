bdddfe994c735cad53324cf46c80704e0f6253e4
AbinayaRajaA <abinaya78mani@gmail.com>
2022-06-18 17:39:51 +0530

    ok

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,24 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+ @Override
+public void deposit() {
+	System.out.println("Deposit Amount----500000");
+}
+ @Override
+	public void deposit(int a) {
+		System.out.println("Deposit Amount---100000+"+a );
+	}
+ @Override
+	public void deposit(long l) {
+		System.out.println("Deposit Amount----2000000+"+l);
+	}
+ public static void main(String[] args) {
+	AxisBank a=new AxisBank();
+	a.deposit();
+	a.deposit(2000000);
+	a.deposit(500000000);
+	a.fixed();
+	a.saving();
+}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,22 @@
+package org.bank;
+
+public class BankInfo  {
+public void saving() {
+	System.out.println("Savings ----56000");
+	
+}
+public void fixed() {
+	System.out.println("Fixed deposit------200000");
+
+}
+public void deposit() {
+	System.out.println("Deposit amount 20000");
+}
+public void deposit(int a) {
+	System.out.println("Deposit Amount----"+a);
+}
+public void deposit(long l) {
+	System.out.println("Deposit Amount----"+l);
+}
+
+}
